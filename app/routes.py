9c2f7a5dd2857f7a361c798aca7546099c7110e5
Steven Hardy <steverhardy@gmail.com>
2019-05-16 14:26:13 -0400

    added API call test routes

--- app/routes.py
+++ app/routes.py
@@ -17,5 +17,116 @@ def index():
     city = 'boston'
 
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}&units=imperial'
-    print(API_KEY)
-    return render_template('index.html')
+
+    # .json() converts string response into Python dictionary
+    response = requests.get(url).json()
+
+    print(response)
+    return render_template('index.html', response=response)
+
+
+@app.route('/api/customers/', methods=['GET', 'POST'])
+def apiCustomers():
+    name = request.args.get('name')
+
+    customers = [
+        {
+            'name': 'John',
+            'age': 22
+        },
+        {
+            'name': 'Alex',
+            'age': 12
+        },
+        {
+            'name': 'Annie',
+            'age': 67
+        },
+        {
+            'name': 'Jake',
+            'age': 45
+        },
+        {
+            'name': 'Bill',
+            'age': 32
+        },
+    ]
+
+    try:
+        for customer in customers:
+            if customer['name'] == name:
+                return jsonify(customer)
+    except:
+        return jsonify({'Error 5000': 'Something went wrong'})
+
+    return jsonify({'Error 304': 'No customer found'})
+
+
+@app.route('/api/customers/age/', methods=['GET', 'POST'])
+def apiCustomersAge():
+    min = request.args.get('min')
+    max = request.args.get('max')
+
+    # print(request.args)
+
+    # name is optional
+    name = request.args.get('name')
+
+    customers = [
+        {
+            'name': 'John',
+            'age': 22
+        },
+        {
+            'name': 'Alex',
+            'age': 12
+        },
+        {
+            'name': 'Annie',
+            'age': 67
+        },
+        {
+            'name': 'Jake',
+            'age': 45
+        },
+        {
+            'name': 'Bill',
+            'age': 32
+        },
+    ]
+
+    try:
+        results = []
+
+        for customer in customers:
+            if int(min) < customer['age'] and customer['age'] < int(max):
+                results.append(customer)
+
+        if name:
+            customers = []
+            for customer in results:
+                if customer['name'] == name:
+                    return jsonify(customer)
+
+        return jsonify(results)
+    except:
+        return jsonify({'Error': 'Incompatible parameter values'})
+
+    return jsonify({'error': 'Something went wrong'})
+
+
+@app.route('/api/coords/', methods=['GET', 'POST'])
+def coords():
+    city = request.headers.get('city')
+
+    API_KEY = app.config['WEATHER_API_KEY']
+
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}&units=imperial'
+
+    try:
+        # json() method converts string response into python dictionary
+        response = requests.get(url).json()
+
+        return jsonify({'city': response['name']})
+    except:
+        return jsonify({'error': 'City does not exist'})
